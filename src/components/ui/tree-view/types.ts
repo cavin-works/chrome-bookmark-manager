6ac7733d2930b7b435b937c6832dcb54c9b2b059
cavinHuang <943866180@qq.com>
2025-06-30 08:19:16 +0800

    feat: 更新 tsconfig.node.json 的模块解析方式为 bundler，新增树形视图组件类型定义

---
+++ src/components/ui/tree-view/types.ts
@@ -0,0 +1,36 @@
+export interface TreeViewItem {
+  id: string;
+  name: string;
+  type: string;
+  children?: TreeViewItem[];
+  checked?: boolean;
+}
+export interface TreeViewMenuItem {
+  id: string;
+  label: string;
+  icon?: Function;
+  action: (items: TreeViewItem[]) => void;
+}
+export interface TreeViewIconMap {
+  [key: string]: Function | undefined;
+}
+export interface TreeViewProps {
+  className?: string;
+  data: TreeViewItem[];
+  title?: string;
+  showExpandAll?: boolean;
+  showCheckboxes?: boolean;
+  checkboxPosition?: "left" | "right";
+  searchPlaceholder?: string;
+  selectionText?: string;
+  checkboxLabels?: {
+    check: string;
+    uncheck: string;
+  };
+  getIcon?: (item: TreeViewItem, depth: number) => Function;
+  onSelectionChange?: (selectedItems: TreeViewItem[]) => void;
+  onAction?: (action: string, items: TreeViewItem[]) => void;
+  onCheckChange?: (item: TreeViewItem, checked: boolean) => void;
+  iconMap?: TreeViewIconMap;
+  menuItems?: TreeViewMenuItem[];
+}
